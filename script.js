7c42928684500f1c3d8b38ce5128ba6cf9884739
MVolodya <medvid.volodya@gmail.com>
2018-12-24 11:18:41 +0200

    changed way of drawing cells

--- script.js
+++ script.js
@@ -3,51 +3,65 @@
   var cw = canvas.width = 300
   var ch = canvas.height = 300
   var ctx = canvas.getContext('2d')
-
-  var resolution = 50
+  var resolution = 30
   var cols = ch / resolution;
   var rows = cw / resolution;
-  var grid = null;
+  var grid = [];
 
-  function make2DArray(cols, rows) {
-    var arr = new Array(cols)
-    for(var i = 0; i < arr.length; i++) {
-      arr[i] = new Array(rows)
-    }
-    return arr
-  }
-
-  function fill2DArray() {
-    grid = make2DArray(cols, rows)
-    for(var i = 0; i < cols; i++) {
-      for(var j = 0; j < rows; j++) {
-        grid[i][j] = 0
+  function Game(cols, rows) {
+    this.cols = cols
+    this.rows = rows
+  
+    var makeGrid = function() {
+      var arr = new Array(this.cols)
+      for(var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(this.rows)
+        for(var j = 0; j < this.rows; j++) {
+          arr[i][j] = 0
+        }
       }
+      return arr
+    }
+
+    this.generation = function() {
+      var nextGeneration = makeGrid()
+    }
+
+    this.startGame = function() {
+      grid = makeGrid.apply(this);
     }
   }
 
-  function drwa2DArray() {
-    ctx.clearRect(0,0,cw,ch)
-    for(var i = 0; i < cols; i++) {
-      for(var j = 0; j < rows; j++) {
-        if(grid[i][j] == 1) {
-          ctx.fillRect(j*resolution, i*resolution, resolution-1, resolution-1);
+  function Cell(x, y) {
+    this.x = x
+    this.y = y
+
+    this.setCell = function() {
+      grid[this.y][this.x] = 1
+      this.draw()
+    }
+
+    this.draw = function() {
+      ctx.clearRect(0, 0, cw, ch)
+      for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+          if(grid[i][j] == 1) {
+            ctx.fillRect(j*resolution, i*resolution, resolution-1, resolution-1);
+          }
         }
       }
     }
   }
 
+
   canvas.onclick = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    x = Math.floor(x/resolution);
-    y = Math.floor(y/resolution);
-    grid[y][x] = 1
-    drwa2DArray()
+    var x = Math.floor(e.offsetX / resolution);
+    var y = Math.floor(e.offsetY / resolution);
+    var cell = new Cell(x, y)
+    cell.setCell()
   }
 
-
-
-  fill2DArray()
+  var newGame = new Game(cols, rows)
+  newGame.startGame()
 
 })()
